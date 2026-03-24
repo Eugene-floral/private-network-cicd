f2d81d0cc2a032d218249909ff03c301b87ddcc6
root <root@devserver.local>
2026-03-24 17:19:08 +0900

    first push

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.json());
+app.use(express.static('public'));
+
+const PORT=5000
+
+//메인 홈 페이지
+
+app.get('/' , (req,res) => {
+	 res.sendFile(path.join(__dirname, '/views', 'index.html'))
+});
+
+//여행 상품 목혹
+app.get('/introduce' , (req,res) => { 
+res.sendFile(path.join(__dirname, '/views' , 'introduce.html'));
+});
+
+app.listen(PORT, ()=> console.log('web is started'));
+
+
+

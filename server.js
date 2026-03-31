20aad74a313bf724a018ca73c4759e53e3e388ad
root <root@devserver.local>
2026-03-31 14:02:25 +0900

    im fucking stupid

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ res.sendFile(path.join(__dirname,'/views' , 'event.html'));
 //허니문-휴양지 목록
 
 app.get('/honeymoon-resort', (req,res) => {
-res.sendFile(path.join(__dirname,'/views' , 'honeymoon-resort'));
+res.sendFile(path.join(__dirname,'/views' , 'honeymoon-resort.html'));
 });
 
 

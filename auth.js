04a5803637e51c583f45837908e52d5e3721c5d2
root <root@devserver.local>
2026-04-09 17:56:34 +0900

    change auth

--- auth.js
+++ auth.js
@@ -2,44 +2,38 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 
-
-
 module.exports = (db) => {
 
-    router.post('/login', (req, res) => {
-	console.log("로그인 요청 왔음", req.body);
-        const {user_id, password } = req.body;
+    // [로그인]
+    router.post('/login', async (req, res) => {
+        console.log("로그인 요청 왔음", req.body);
+        const { user_id, password } = req.body;
         const sql = "SELECT * FROM users WHERE user_id = ?";
 
-        db.query(sql, [user_id], async(err, results) => {
-            if (err) return res.status(500).send("데이터베이스 오류");
-
+        try {
+            const [results] = await db.execute(sql, [user_id]);
 
             if (results.length > 0) {
-		const user = results[0];
-
-		try{
-
-			const match = await bcrypt.compare(password, user.password);
-			if(match){
-               			req.session.user = user;
-                		return res.send("<script>alert('반갑습니다, " + results[0].name + "님!'); location.href='/';</script>");
-            		} else 	{
-               			return res.send("<script>alert('아이디 또는 비밀번호가 틀렸습니다.'); history.back();</script>");
-            		}
-		} catch (compareErr) {
-			console.error("비교에러" ,compareErr);
-			return res.status(500).send("서버오류");
-		}
-	    } else {
-			res.send("<script>alert('id not found'); history.back();</script>");
-		}
-        });
+                const user = results[0];
+                const match = await bcrypt.compare(password, user.password);
+                if (match) {
+                    req.session.user = user;
+                    return res.send("<script>alert('반갑습니다, " + user.name + "님!'); location.href='/';</script>");
+                } else {
+                    return res.send("<script>alert('아이디 또는 비밀번호가 틀렸습니다.'); history.back();</script>");
+                }
+            } else {
+                return res.send("<script>alert('id not found'); history.back();</script>");
+            }
+        } catch (err) {
+            console.error("로그인 에러:", err);
+            return res.status(500).send("서버 오류");
+        }
     });
 
     // [로그아웃]
     router.get('/logout', (req, res) => {
-        req.session.destroy(); // 세션 삭제
+        req.session.destroy();
         res.send("<script>alert('로그아웃 되었습니다.'); location.href='/';</script>");
     });
 
